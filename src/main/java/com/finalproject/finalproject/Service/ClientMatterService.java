90ecce99c67263478a23fa9ae19aa3d5fdd294b4
Jeff Haupt <66330433+jeff1haupt@users.noreply.github.com>
2020-09-26 17:01:42 -0400

    Updating Service Classes

--- src/main/java/com/finalproject/finalproject/Service/ClientMatterService.java
+++ src/main/java/com/finalproject/finalproject/Service/ClientMatterService.java
@@ -1,7 +1,39 @@
 package com.finalproject.finalproject.Service;
 
+import com.finalproject.finalproject.Repository.ClientMatterRepository;
+import com.finalproject.finalproject.Repository.ClientRepository;
+import com.finalproject.finalproject.Repository.LawyerRepository;
+import com.finalproject.finalproject.entity.Client;
+import com.finalproject.finalproject.entity.ClientMatter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
 public class ClientMatterService {
 
+    @Autowired
+    private ClientMatterRepository repo;
 
+    @Autowired
+    private LawyerRepository lawyerRepo;
+
+    @Autowired
+    private ClientRepository clientRepo;
+
+    public ClientMatter saveNewMatter() {
+
+    }
+
+    private ClientMatter initializeNewMatter(Set<Long> productIds, Client client) {
+        ClientMatter matter = new ClientMatter();
+        matter.setClient(client);
+        matter.setLawyer(Set<Lawyer> Lawyer);
+        matter.setPracticeAreas(PracticeArea);
+        //matter.setHourly(boolean hourly);
+        //matter.setDateOpened(Date dateOpened);
+        return matter;
+    }
 
 }
--- src/main/java/com/finalproject/finalproject/Service/ClientService.java
+++ src/main/java/com/finalproject/finalproject/Service/ClientService.java
@@ -1,4 +1,29 @@
 package com.finalproject.finalproject.Service;
 
+import com.finalproject.finalproject.Repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.finalproject.finalproject.entity.Client;
+
+import java.util.Optional;
+
+@Service
 public class ClientService {
+
+    @Autowired
+    private ClientRepository repo;
+
+    public Client createClient(Client client) {
+        return repo.save(client);
+    }
+
+    public Client updateClient(Long clientId) throws Exception {
+        Optional<Client> client = repo.findById(clientId);
+        if ( client.isEmpty() ) {
+            throw new Exception( "Client does not exist." );
+        }
+        
+    }
+
 }
